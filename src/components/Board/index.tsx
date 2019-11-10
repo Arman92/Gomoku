141f671a7fb6b1c2ffe097275d12d402a0002912
Arman <Arman1371@gmail.com>
2019-11-10 09:36:12 +0330

    Added Material-UIs, UI improvement

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,19 +1,93 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
-import { BoardContainer } from "./styled";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Slide from "@material-ui/core/Slide";
+
+import {
+  BoardContainer,
+  GameContainer,
+  RightContainer,
+  Header,
+  Content
+} from "./styled";
 import Game from "./components/Game";
 
+const Transition = React.forwardRef((props, ref) => {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 const BoardComponent: FC = () => {
+  const [open, setOpen] = useState(false);
+
+  const onGameFinished = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <BoardContainer>
-      <Game />
-      <button
-        onClick={() => {
-          // dispatchGameBoard({ type: "undo" });
-        }}
-      >
-        Undo
-      </button>
+      <Header>
+        <h1>Gobang</h1>
+      </Header>
+      <Content>
+        <GameContainer>
+          <Dialog
+            open={open}
+            TransitionComponent={Transition}
+            keepMounted={true}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-slide-title"
+            aria-describedby="alert-dialog-slide-description"
+          >
+            <DialogTitle id="alert-dialog-slide-title">
+              {"Use Google's location service?"}
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-slide-description">
+                Let Google help apps determine location. This means sending
+                anonymous location data to Google, even when no apps are
+                running.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                Disagree
+              </Button>
+              <Button onClick={handleClose} color="primary">
+                Agree
+              </Button>
+            </DialogActions>
+          </Dialog>
+
+          <Game onGameFinished={onGameFinished} />
+        </GameContainer>
+        <RightContainer>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              // dispatchGameBoard({ type: "undo" });
+            }}
+          >
+            New Game
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              // dispatchGameBoard({ type: "undo" });
+            }}
+          >
+            Undo
+          </Button>
+        </RightContainer>
+      </Content>
     </BoardContainer>
   );
 };
--- src/components/Board/styled.ts
+++ src/components/Board/styled.ts
@@ -5,9 +5,48 @@ import boardBg from "@gobang/assets/imgs/2551066.jpg";
 export const BoardContainer = styled.div`
   width: 90vw;
   height: 90vw;
-  max-width: 90vh;
+  max-width: 120vh;
   max-height: 90vh;
 
   border-radius: 1rem;
   background-image: url(${boardBg});
+
+  display: flex;
+  flex-direction: column;
+  user-select: none;
+  box-shadow: inset 0 0 3px #ffdebe, 1px 2px 7px #2d2d2d;
+`;
+
+export const Header = styled.header`
+  text-align: center;
+  color: #9c5321;
+  text-shadow: 0 1px 1px rgba(255, 255, 255, 0.5), 0 -1px 1px rgba(0, 0, 0, 0.5);
+
+  h1 {
+    font-size: 2.5rem;
+    font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+    letter-spacing: 0.3rem;
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+`;
+
+export const GameContainer = styled.div`
+  flex: 0.9;
+`;
+
+export const RightContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 0.3;
+  justify-content: center;
+  align-items: stretch;
+
+  button {
+    margin: 2rem 1rem;
+  }
 `;
